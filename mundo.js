24be36fc6fadaec77ecf7813cb493b319e4bd91a
Cam <herreracamila1304@gmail.com>
2026-06-21 22:33:21 -0300

    [ADD] estructura base del proyecto

---
+++ mundo.js
@@ -0,0 +1,32 @@
+class Mundo {
+    constructor() {
+        this.escenas = [];
+        this.escenaActual = null;
+        this.indiceActual = null;
+    }
+
+    addEscena(e) {
+        this.escenas.push(e);
+        if (this.indiceActual == null) this.elegirEscena = 0;
+    }
+
+    elegirEscena(i) {
+        if (i < 0 || i >= this.escenas.length) {
+            this.escenaActual = 0; //en caso de algun error vuelve a la pantalla de inicio
+        }
+
+        this.escenaActual = this.escenas[i];
+        this.indiceActual = i;
+    }
+
+    escenaSiguiente() {
+        let i = (this.indiceActual + 1) % this.escenas.length;
+        this.elegirEscena(i);
+    }
+
+    escenaPrevia() {
+        let i = this.indiceActual - 1;
+        if (i < 0) i = this.escenas.length - 1;
+        this.elegirEscena(i);
+    }
+}
---
+++ sketch.js
@@ -0,0 +1,18 @@
+const mundo = new Mundo();
+ 
+function preload(){
+//ingresar cosas que se cargan previamente
+}
+
+function setup() {
+  createCanvas(400, 400);
+
+//hacemos un push dentro de mundo con cada escena llamando a 'addEscena'
+  mundo.addEscena(new PantallaInicio());
+  mundo.addEscena(new Nivel_01());
+  mundo.addEscena(new PantallaFinal());
+}
+
+function draw() {
+  background(220);
+}
