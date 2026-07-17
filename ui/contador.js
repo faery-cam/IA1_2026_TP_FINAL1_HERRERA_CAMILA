51fa2e81a4c89060117a6f9d334f6f1ef6d4903b
Cam <herreracamila1304@gmail.com>
2026-07-17 01:19:02 -0300

    [UPDATE] contador mejorado

--- ui/contador.js
+++ ui/contador.js
@@ -5,17 +5,26 @@ class CuentaAtras {
     }
 
     update() {
-        if (millis() - this.tpoContar >= 1000) {
-            this.num--;
-            this.tpoContar = millis();
-            if (this.num <= 0) {
-                return true;
-            }
+        if (millis() - this.tpoContar < 1000) {
+            this.num = 3;
+        } else if (millis() - this.tpoContar < 2000) {
+            this.num = 2;
+        } else if (millis() - this.tpoContar < 3000) {
+            this.num = 1;
+        } else {
+            return true;
         }
     }
 
     draw() {
+        textSize(40);
+        fill(255);
+
         text(this.num, width / 2, height / 2);
+
+        textSize(20);
+        text(millis() - this.tpoContar, width / 2, height / 2 + 40);
+
     }
 
     reiniciar() {
